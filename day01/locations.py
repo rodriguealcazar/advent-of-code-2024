4066ff4272368348e745fbaddf7fd84f3c6c260a
Rodrigue Alcazar <rodrigue@komoot.de>
2024-12-03 21:30:05 +0100

    Day 1, part 2

---
+++ day01/locations.py
@@ -0,0 +1,44 @@
+import argparse
+from collections import Counter
+
+
+def main(input: str, part: int):
+    with open(input, "r") as f:
+        list_1 = []
+        list_2 = []
+        for pair in f:
+            if pair := pair.strip():
+                a, b = pair.split("  ")
+                list_1.append(int(a))
+                list_2.append(int(b))
+
+    match part:
+        case 1:
+            total_distance(list_1, list_2)
+        case 2:
+            total_similarity(list_1, list_2)
+
+
+def total_distance(list_1: list[int], list_2: list[int]):
+    distance = 0
+    for a, b in zip(sorted(list_1), sorted(list_2)):
+        distance += abs(a - b)
+    print(distance)
+
+
+def total_similarity(list_1: list[int], list_2: list[int]):
+    counts = Counter(list_2)
+    similarity = 0
+    for loc in list_1:
+        similarity += loc * counts[loc]
+
+    print(similarity)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", "-i")
+    parser.add_argument("--part", "-p", type=int, default=1)
+    args = parser.parse_args()
+
+    main(args.input, args.part)
